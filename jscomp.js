39a139f26f095b22c7fac81fb39efce366b766ca
Ezel Inan <inanezel@Outlook.com>
2024-04-22 17:32:49 +0000

    JSCompliment

---
+++ jscomp.js
@@ -0,0 +1,7 @@
+var whatList = ["hair", "face", "figure", "brain", "taste in music", "ability to code in Javascript"];	  	   	   	    	      	   	  	
+var descriptionList = ["pretty", "intelligent", "beautiful", "astute", "like tasting a rainbow"];	  	   	   	    	      	   	  	
+var betterthanList = ["a weekend for two in the Pacific", "a gold medal on a Test", "the baldness of a Dinosaur", "a slap up meal in McDonalds at the Parade"];	  	   	   	    	      	   	  	
+var a = Math.floor(Math.random()*6);	  	   	   	    	      	   	  	
+var b = Math.floor(Math.random()*5);	  	   	   	    	      	   	  	
+var c = Math.floor(Math.random()*4);	  	   	   	    	      	   	  	
+console.log("Your "+whatList[a]+" is "+descriptionList[b]+" and better than "+betterthanList[c]);
